1650ed94c2afa4a2d41367a319cb8e90e755d2f1
ARI VPN STORES <arivpnstores@gmail.com>
2026-03-21 15:04:04 +0700

    update new qris gopay merchat

--- update.sh
+++ update.sh
@@ -26,25 +26,25 @@ npm install -g pm2
     if [ -n "$(ls -A /root/BotVPN)" ]; then
         chmod +x /root/BotVPN/*
     fi
-wget -O .gitattributes "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/.gitattributes"
-wget -O README.md "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/README.md"
-wget -O app.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/app.js"
-wget -O wd.py "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/wd.py"
-wget -O cek-port.sh "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/cek-port.sh"
-wget -O ecosystem.config.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/ecosystem.config.js"
-wget -O package.json "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/package.json"
-wget -O ss.png "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/ss.png"
-wget -O ss2.png "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/ss2.png"
-wget -O start "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/start"
-wget -O update.sh "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/update.sh"
-wget -O /root/BotVPN/modules/reseller.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/reseller.js"
-wget -O /root/BotVPN/modules/change-ip.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/change-ip.js"
-wget -O /root/BotVPN/modules/create.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/create.js"
-wget -O /root/BotVPN/modules/del.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/del.js"
-wget -O /root/BotVPN/modules/lock.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/lock.js"
-wget -O /root/BotVPN/modules/unlock.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/unlock.js"
-wget -O /root/BotVPN/modules/renew.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/renew.js"
-wget -O /root/BotVPN/modules/trial.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/trial.js"
+ wget --connect-timeout=1 --timeout=30 -O .gitattributes "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/.gitattributes"
+ wget --connect-timeout=1 --timeout=30 -O README.md "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/README.md"
+ wget --connect-timeout=1 --timeout=30 -O app.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/app.js"
+ wget --connect-timeout=1 --timeout=30 -O wd.py "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/wd.py"
+ wget --connect-timeout=1 --timeout=30 -O cek-port.sh "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/cek-port.sh"
+ wget --connect-timeout=1 --timeout=30 -O ecosystem.config.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/ecosystem.config.js"
+ wget --connect-timeout=1 --timeout=30 -O package.json "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/package.json"
+ wget --connect-timeout=1 --timeout=30 -O ss.png "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/ss.png"
+ wget --connect-timeout=1 --timeout=30 -O ss2.png "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/ss2.png"
+ wget --connect-timeout=1 --timeout=30 -O start "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/start"
+ wget --connect-timeout=1 --timeout=30 -O update.sh "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/update.sh"
+ wget --connect-timeout=1 --timeout=30 -O /root/BotVPN/modules/reseller.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/reseller.js"
+ wget --connect-timeout=1 --timeout=30 -O /root/BotVPN/modules/change-ip.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/change-ip.js"
+ wget --connect-timeout=1 --timeout=30 -O /root/BotVPN/modules/create.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/create.js"
+ wget --connect-timeout=1 --timeout=30 -O /root/BotVPN/modules/del.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/del.js"
+ wget --connect-timeout=1 --timeout=30 -O /root/BotVPN/modules/lock.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/lock.js"
+ wget --connect-timeout=1 --timeout=30 -O /root/BotVPN/modules/unlock.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/unlock.js"
+ wget --connect-timeout=1 --timeout=30 -O /root/BotVPN/modules/renew.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/renew.js"
+ wget --connect-timeout=1 --timeout=30 -O /root/BotVPN/modules/trial.js "https://raw.githubusercontent.com/arivpnstores/BotVPN/main/modules/trial.js"
 
 # stop dulu servicenya
 systemctl stop sellvpn.service
@@ -92,7 +92,7 @@ DB_FILES=("sellvpn.db" "trial.db" "ressel.db")
 for DB_FILE in "${DB_FILES[@]}"; do
     FILE_PATH="$DB_FOLDER/$DB_FILE"
     if [ -f "$FILE_PATH" ]; then
-        curl -s -F chat_id="$USER_ID" \
+        curl -sS --connect-timeout 1 --max-time 30 --fail -F chat_id="$USER_ID" \
              -F document=@"$FILE_PATH" \
              "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" >/dev/null 2>&1
         echo "✅ $DB_FILE terkirim ke Telegram"
@@ -142,22 +142,22 @@ service cron restart
             echo -e "${red}DATA QRIS tidak boleh kosong. Silakan coba lagi.${neutral}"
         fi
     done
-    while [ -z "$auth_username_mutasi" ]; do
-        read -p "Masukkan auth_username_mutasi : " auth_username_mutasi
-        if [ -z "$auth_username_mutasi" ]; then
-            echo -e "${red}auth_username_mutasi tidak boleh kosong. Silakan coba lagi.${neutral}"
+    while [ -z "$GOPAY_KEY" ]; do
+        read -p "Masukkan GOPAY_KEY : " GOPAY_KEY
+        if [ -z "$GOPAY_KEY" ]; then
+            echo -e "${red}GOPAY_KEY tidak boleh kosong. Silakan coba lagi.${neutral}"
         fi
     done
-    while [ -z "$auth_token_mutasi" ]; do
-        read -p "Masukkan auth_token_mutasi : " auth_token_mutasi
-        if [ -z "$auth_token_mutasi" ]; then
-            echo -e "${red}auth_token_mutasi tidak boleh kosong. Silakan coba lagi.${neutral}"
+    while [ -z "$MERCHANT_ID" ]; do
+        read -p "Masukkan MERCHANT_ID : " MERCHANT_ID
+        if [ -z "$MERCHANT_ID" ]; then
+            echo -e "${red}MERCHANT_ID tidak boleh kosong. Silakan coba lagi.${neutral}"
         fi
     done
-    while [ -z "$auth_paymet_getway" ]; do
-        read -p "Masukkan auth_paymet_getway : " auth_paymet_getway
-        if [ -z "$auth_paymet_getway" ]; then
-            echo -e "${red}auth_paymet_getway tidak boleh kosong. Silakan coba lagi.${neutral}"
+    while [ -z "$AUTH_PAYMET_GETWAY" ]; do
+        read -p "Masukkan AUTH_PAYMET_GETWAY : " AUTH_PAYMET_GETWAY
+        if [ -z "$AUTH_PAYMET_GETWAY" ]; then
+            echo -e "${red}AUTH_PAYMET_GETWAY tidak boleh kosong. Silakan coba lagi.${neutral}"
         fi
     done
     while [ -z "$web_mutasi" ]; do
@@ -180,9 +180,9 @@ service cron restart
   \"GROUP_ID\": \"$groupid\",
   \"PORT\": \"6969\",
   \"DATA_QRIS\": \"$dataqris\",
-  \"auth_username_mutasi\": \"$auth_username_mutasi\",
-  \"auth_token_mutasi\": \"$auth_token_mutasi\",
-  \"auth_paymet_getway\": \"$auth_paymet_getway\",
+  \"GOPAY_KEY\": \"$GOPAY_KEY\",
+  \"MERCHANT_ID\": \"$MERCHANT_ID\",
+  \"AUTH_PAYMET_GETWAY\": \"$AUTH_PAYMET_GETWAY\",
   \"web_mutasi\": \"$web_mutasi\"
 }" >/root/BotVPN/.vars.json
 
